fc598c5791769052c23c79ebfbb969a7d1fc50c1
Sergiy Melnychuk <serh.mel@gmail.com>
2026-05-05 12:53:20 +0300

    optimize background render

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useCallback, useEffect, useRef, useState } from "react";
 import { Header, Footer } from "./components/layout";
 import { Home, About, Projects, Experience } from "./components/sections";
 import ScrollToTopButton from "./components/ui/scroll-to-top-button.jsx";
+import bg from "/bg-texture.webp";
 
 function App() {
   const [scrolled, setScrolled] = useState(false);
@@ -23,7 +24,9 @@ function App() {
   }, []);
 
   return (
-    <main className="ms-back scroll-smooth font-sans text-gray-800">
+    <main
+      className="scroll-smooth font-sans text-gray-800 bg-black bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${bg})` }}>
       <Header scrolled={ scrolled } />
       <Home onScrollDown={ onScrollToAbout }/>
       <About ref={ aboutRef }/>
